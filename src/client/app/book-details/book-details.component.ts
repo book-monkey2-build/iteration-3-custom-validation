68b766cf6d590632c9d63dd50d200db3d7decd99
The Buildbot <buildbot@angular2buch.de>
2016-04-20 15:16:04 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Angular2Buch/book-monkey2/commit/3d99884d840cc5a6f121c62c4cb556c3dbc23980
    Travis build: https://travis-ci.org/Angular2Buch/book-monkey2/builds/124499606

--- src/client/app/book-details/book-details.component.ts
+++ src/client/app/book-details/book-details.component.ts
@@ -1,21 +1,30 @@
 import {Component, Input} from 'angular2/core';
-import {RouteParams} from 'angular2/router';
+import {Router, RouteParams, ROUTER_DIRECTIVES} from 'angular2/router';
 import {Book} from '../../domain/book';
 import {BookStoreService} from '../services/books/book-store.service';
 
 @Component({
   selector: 'book-details',
-  templateUrl: '/app/book-monkey/iteration-3/validation/book-details/book-details.html',
-  providers: [BookStoreService]
+  moduleId: module.id,
+  templateUrl: 'book-details.html',
+  providers: [BookStoreService],
+  directives: [ROUTER_DIRECTIVES]
 })
 export class BookDetailsComponent {
   book: Book;
 
-  constructor(private params: RouteParams, private bs: BookStoreService) {
+  constructor(private params: RouteParams, private bs: BookStoreService, private router: Router) {
     this.book = bs.getSingle(params.get('isbn'));
   }
 
   getRating(num: number){
     return new Array(num);
+  }
+
+  deleteBook(){
+    if(confirm("Buch wirklich löschen?")){
+       this.bs.delete(this.params.get('isbn'))
+       this.router.navigate(['../List']);
+    }
   }
 }
